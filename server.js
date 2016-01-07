004162ea94918302b00ba121787faffc438f069d
Martin B <brusebox2000@gmail.com>
2016-01-07 08:00:31 -0500

    changes to allow for md5 checking and file positioning

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http'),
     scp = require('scp'),
-    argv = require('yargs').argv;
+    argv = require('yargs').argv,
+    md5 = require('md5-file'),
+    path = require('path');
 
 var sourceHost;
 var sourcePort;
@@ -15,19 +17,37 @@ if(argv._[0] && argv._[1]){
 }
 
 const PORT = 8080;
+const FILEDIR = '/root/transfers/';
     
 function handleRequest(request, response){
     if(request.headers.filename && request.headers.hash){
         transferFilePath = request.headers.filename;
+	fmd5 = request.headers.hash;
         scp.get({
             file:transferFilePath,
             user:'root',
             host:sourceHost,
             port:sourcePort,
-            path: '~'
-        });
+            path: FILEDIR
+        }, function(err, stdout, stderr){
+	   if (err){
+		response.writeHead(400, {'Content-Type': 'text/plain'});
+		response.write(stderr);
+		console.log('Error');
+	   }else{
+		var baseFile =  FILEDIR + path.basename(transferFilePath);
+		console.log(md5(baseFile.toString()));
+		console.log(fmd5);
+		if(md5(baseFile.toString()) === fmd5){
+		    console.log('WAITING');
+		    response.writeHead(111, {'Content-Type': 'text/plain'} );
+		    response.write('Transfer successful');
+		    response.end();
+      		    console.log('Success');
+		}
+	   }
+	});
     } 
-    response.end('Hit ');
 }
 
 var server = http.createServer(handleRequest);
