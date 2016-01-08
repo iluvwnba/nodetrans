b4d4693a8b98a5eaac04e6b6ed95e4bccc7f643b
Martin Barnes <brusebox2000@gmail.com>
2016-01-08 09:36:59 +0000

    Added basic client

---
+++ client.js
@@ -0,0 +1,57 @@
+/**
+ * Created by mb on 08/01/2016.
+ */
+
+var http = require('http'),
+    argv = require('yargs').argv,
+    md5 = require('md5-file'),
+    chokidar = require('chokidar'),
+    destinationHost, destinationPort, watchDir;
+
+
+if(argv._[0] && argv._[1]){
+    destinationHost = argv._[0];
+    destinationPort = argv._[1];
+}else{
+    console.log('Incorrect parameters');
+    return 1;
+}
+
+
+var watcher = chokidar.watch(watchDir, {
+    ignored: /[\/\\]\./,
+    persistent: true
+});
+
+
+
+callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+        console.log(str);
+    });
+};
+
+// Add event listeners.
+watcher.on('add', function(path){
+    var fmd5 = md5(path);
+    var options = {
+        host: destinationHost,
+        port: destinationPort,
+        headers:{
+            host: destinationHost,
+            port: destinationPort,
+            filename: path,
+            hash: fmd5
+        }
+    };
+    http.request(options, callback).end();
+});
+
