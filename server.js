13cb5774d26c3573604c927d35f250be3c99f09e
martin <brusebox2000@gmail.com>
2016-01-14 04:47:48 -0500

    checksum check

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var http = require('http'),
-    scp = require('scp2'),
     argv = require('yargs').argv,
-    md5 = require('md5'),
+    md5 = require('checksum'),
     path = require('path'),
-    fs = require('fs');
-
-var Rsync = require('rsync');
+    fs = require('fs'),
+	SFTPS = require('sftps');
 
 
 var sourceHost;
 var sourcePort;
 var transferFilePath;
+
 
 if (argv._[0] && argv._[1]) {
     sourceHost = argv._[0];
@@ -23,41 +22,53 @@ if (argv._[0] && argv._[1]) {
 const PORT = 8080;
 const FILEDIR = '/root/transfers/';
 
+var sftp = new SFTPS({
+	host: '10.131.143.93', // required
+	username: 'root', // required
+	password: process.env.NODEPASS// required
+	port: 22 // optional
+});
+
+
 function handleRequest(request, response) {
     if (request.headers.filename && request.headers.hash) {
         transferFilePath = request.headers.filename;
         fmd5 = request.headers.hash;
-       var rsync = new Rsync()
-            .shell('ssh')
-            .flags('az')
-            .source(sourceHost + ':' + transferFilePath)
-            .destination(FILEDIR);
-        rsync.execute(
-            function (err) {
-            if (err) {
-                response.writeHead(400, {'Content-Type': 'text/plain'});
-                console.log('Error: SCP FAILED ON ' + transferFilePath);
-		console.log(err);
-            } else {
-                var actualmd5 = '';
+		
+		sftp.get(transferFilePath, transferFilePath).exec(function(err, res){
+			if(!err){
+				fs.exists(transferFilePath, function(exists) {
+					console.log(transferFilePath + '   exists');
+					if (exists) {
+					var actualmd5 = '';
+					fs.readFile(transferFilePath, function (err, buf) {
+						if (err) {
+							console.log('Couldnt read file');
+							console.log(err);
+						} else {
+							actualmd5 = md5(buf);
+							console.log(md5(buf));
 
-                fs.readFile(FILEDIR + path.basename(transferFilePath), function (err, buf) {
-                        if (err) {
-                            console.log('Couldnt read file');
-                console.log(err);
-                        } else {
-                            actualmd5 = md5(buf);
-                            if (actualmd5 === fmd5) {
-                                response.writeHead(200, {'Content-Type': 'text/plain'});
-                                response.write('Transfer successful');
-                                response.end();
-                                console.log('Success');
-                            }
-                        }
-                    });
+							if (actualmd5 === fmd5) {
+								response.writeHead(200, {'Content-Type': 'text/plain'});
+								response.write('Transfer successful');
+								response.end();
+								console.log('Success');
+							}else{
+								response.writeHead(400, {'Content-Type': 'text/plain'});
+								response.write('MD5 check unsuccessful');
+								response.end();
+								console.log(transferFilePath + ' transfered');
+							}
+						}
+					});
+				}	
+                });
+			}else{
+				console.log(transferFilePath + ' not transfered');
+			}
+		});		
 
-            }
-        });
     }
 }
 
