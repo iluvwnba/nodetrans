1a4ae603490bc3a7e6b4bfc8c4336765f36689c0
Martin Barnes <brusebox2000@gmail.com>
2016-01-14 09:54:07 +0000

    added new checksum method

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var http = require('http'),
     argv = require('yargs').argv,
-    md5 = require('checksum'),
-    path = require('path'),
+    checksum = require('checksum'),
     fs = require('fs'),
-	SFTPS = require('sftps');
+    SFTPS = require('sftps');
 
 
 var sourceHost;
@@ -23,10 +22,10 @@ const PORT = 8080;
 const FILEDIR = '/root/transfers/';
 
 var sftp = new SFTPS({
-	host: '10.131.143.93', // required
-	username: 'root', // required
-	password: process.env.NODEPASS// required
-	port: 22 // optional
+    host: '10.131.143.93', // required
+    username: 'root', // required
+    password: process.env.NODEPASS, // required
+    port: 22 // optional
 });
 
 
@@ -34,41 +33,33 @@ function handleRequest(request, response) {
     if (request.headers.filename && request.headers.hash) {
         transferFilePath = request.headers.filename;
         fmd5 = request.headers.hash;
-		
-		sftp.get(transferFilePath, transferFilePath).exec(function(err, res){
-			if(!err){
-				fs.exists(transferFilePath, function(exists) {
-					console.log(transferFilePath + '   exists');
-					if (exists) {
-					var actualmd5 = '';
-					fs.readFile(transferFilePath, function (err, buf) {
-						if (err) {
-							console.log('Couldnt read file');
-							console.log(err);
-						} else {
-							actualmd5 = md5(buf);
-							console.log(md5(buf));
 
-							if (actualmd5 === fmd5) {
-								response.writeHead(200, {'Content-Type': 'text/plain'});
-								response.write('Transfer successful');
-								response.end();
-								console.log('Success');
-							}else{
-								response.writeHead(400, {'Content-Type': 'text/plain'});
-								response.write('MD5 check unsuccessful');
-								response.end();
-								console.log(transferFilePath + ' transfered');
-							}
-						}
-					});
-				}	
+        sftp.get(transferFilePath, transferFilePath).exec(function (err, res) {
+            if (!err) {
+                fs.exists(transferFilePath, function (exists) {
+                    console.log(transferFilePath + '   exists');
+                    if (exists) {
+                        checksum.file(transferFilePath, function (err, sum) {
+                            if (sum === fmd5) {
+                                response.writeHead(200, {'Content-Type': 'text/plain'});
+                                response.write('Transfer successful');
+                                response.end();
+                                console.log('Success');
+                            }else{
+                                response.writeHead(400, {'Content-Type': 'text/plain'});
+                                response.write('MD5 check unsuccessful');
+                                response.end();
+                                console.log(transferFilePath + ' transfered');
+                            }
+                        });
+                    }else{
+                        console.log(transferFilePath + ' does not exist');
+                    }
                 });
-			}else{
-				console.log(transferFilePath + ' not transfered');
-			}
-		});		
-
+            } else {
+                console.log(transferFilePath + ' not transfered');
+            }
+        });
     }
 }
 
